b374f331dade4fa8695d6893bb52b00b57ee05f2
kasunweerasinghe <kasunrachintha2018@gmail.com>
2023-02-13 18:39:33 +0530

    add Car entity class

---
+++ Back_End/src/main/java/lk/ijse/spring/entity/Car.java
@@ -0,0 +1,46 @@
+/**
+ * created by kasun weerasinghe
+ * Date: 2023-02-13
+ * Time: 18:38
+ * Project Name: Back_End
+ */
+
+package lk.ijse.spring.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Entity
+public class Car {
+    @Id
+    private String registrationNO; //car no plate
+    private String brand;
+    private String type;
+    private int noOfPassengers;
+    private String transmissionType;
+    private String fuelType;
+    private String color;
+    private String frontViewImg;
+    private String backViewImg;
+    private String sideViewImg;
+    private String internalViewImg;
+    private double dailyRate;
+    private double monthlyRate;
+    private double freeKmForPrice;
+    private double freeKmForDuration;
+    private double lossDamageWaiver;
+    private double priceForExtraKm;
+    private double completeKm;
+    private String status;
+
+
+}
---
+++ Back_End/src/main/java/lk/ijse/spring/entity/CarRent.java
@@ -0,0 +1,11 @@
+/**
+ * created by kasun weerasinghe
+ * Date: 2023-02-13
+ * Time: 18:39
+ * Project Name: Back_End
+ */
+
+package lk.ijse.spring.entity;
+
+public class CarRent {
+}
