168009c656224d9f8a0cf936d81c0b31e5439edd
kasunweerasinghe <kasunrachintha2018@gmail.com>
2023-02-13 17:41:01 +0530

    block tab key and add function for Check Login validation

--- Front_End/assets/scripts/LoginController.js
+++ Front_End/assets/scripts/LoginController.js
@@ -7,3 +7,38 @@ let regEmail = /^[a-z0-9]{3,}(@)[a-z]{3,}(.)[a-z]{2,3}$/;
 let regDrivingLicenceNo = /^(B)[0-9]{7}$/;
 let regNicNo = /^[0-9]{9}(V)|[0-9]{12}$/;
 
+let baseUrl = "http://localhost:8080/Back_End_war/";
+
+//block tab key
+$('#txtUserName,#txtPassword,#inputName,#inputContactNo,#inputAddress,#inputEmail,#inputDrivingLicence,#inputNIC,#inputUserName,#inputPassword,#inputfile1,#inputfile2,#inputfile3').on('keydown', function (event) {
+    if (event.key == "Tab") {
+        event.preventDefault();
+    }
+});
+
+$('#txtUserName,#txtPassword').on('blur', function () {
+    addLoginFormValidation();
+});
+
+//Check Login validation
+function addLoginFormValidation() {
+    var username = $("#txtUserName").val();
+    if (regLoginUsername.test(username)) {
+        $("#txtUserName").css('border', '2px solid green');
+        $("#userNameError").text("");
+        var password = $('#txtPassword').val();
+        if (regLoginPassword.test(password)) {
+            $("#txtPassword").css('border', '2px solid green');
+            $("#passwordError").text("");
+            return true;
+        } else {
+            $("#txtPassword").css('border', '2px solid red');
+            $("#passwordError").text("Password is not valid. Enter valid password.");
+            return false;
+        }
+    } else {
+        $("#txtUserName").css('border', '2px solid red');
+        $("#userNameError").text("Username is not valid. Enter valid username");
+        return false;
+    }
+}
