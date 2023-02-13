4fb1e4b02996e00a72846cac8f16a1c0331d0e47
kasunweerasinghe <kasunrachintha2018@gmail.com>
2023-02-13 16:26:29 +0530

    content update

--- Front_End/assets/scripts/HomeController.js
+++ Front_End/assets/scripts/HomeController.js
@@ -2,6 +2,20 @@ $(function (){
     loadCarDetails();
 });
 
+let baseUrl = "http://localhost:8080/Back_End_war/";
+
 function loadCarDetails(){
+    $('#divGeneral').empty();
+    $('#divPremium').empty();
+    $('#divLuxury').empty();
+    $.ajax({
+        url:baseUrl + "api/v1/car",
+        method:"GET",
+        success:function (res){
+
+        }
+    });
+
+
 
 }
