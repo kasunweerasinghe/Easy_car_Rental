b522fd751733eba08c4eeff74cb2d1803c6b36c2
kasunweerasinghe <kasunrachintha2018@gmail.com>
2023-02-13 19:32:20 +0530

    added controller classess

---
+++ Back_End/src/main/java/lk/ijse/spring/controller/CarRentController.java
@@ -0,0 +1,18 @@
+/**
+ * created by kasun weerasinghe
+ * Date: 2023-02-13
+ * Time: 19:27
+ * Project Name: Back_End
+ */
+
+package lk.ijse.spring.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/CarRent")
+@CrossOrigin
+public class CarRentController {
+}
