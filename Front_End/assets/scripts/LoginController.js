69f1be1fadd328ceeccb52c47a4d4cc4bb19b7a1
kasunweerasinghe <kasunrachintha2018@gmail.com>
2023-02-13 17:36:57 +0530

    add reg validation

---
+++ Front_End/assets/scripts/LoginController.js
@@ -0,0 +1,9 @@
+let regLoginUsername = /^[A-z0-9]{6,10}$/;
+let regLoginPassword = /^[A-z0-9@#$%&!*]{8,}$/;
+let regName = /^[A-z .]{3,}$/;
+let regAddress = /^[A-z ,.0-9]{3,}$/;
+let regContactNo = /^(0)[1-9][0-9][0-9]{7}$/;
+let regEmail = /^[a-z0-9]{3,}(@)[a-z]{3,}(.)[a-z]{2,3}$/;
+let regDrivingLicenceNo = /^(B)[0-9]{7}$/;
+let regNicNo = /^[0-9]{9}(V)|[0-9]{12}$/;
+
