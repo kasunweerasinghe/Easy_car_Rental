8983695b59b028ee484332efad8e08920a4c7512
kasunweerasinghe <kasunrachintha2018@gmail.com>
2023-02-14 14:45:15 +0530

    added LoginRepo interface

---
+++ Back_End/src/main/java/lk/ijse/spring/service/impl/LoginServiceImpl.java
@@ -0,0 +1,11 @@
+/**
+ * created by kasun weerasinghe
+ * Date: 2023-02-14
+ * Time: 14:44
+ * Project Name: Back_End
+ */
+
+package lk.ijse.spring.service.impl;
+
+public class LoginServiceImpl {
+}
