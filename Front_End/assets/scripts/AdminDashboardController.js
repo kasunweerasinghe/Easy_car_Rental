e07f5a2b32b6fdb1c11a5445898c37dee58326e9
kasunweerasinghe <kasunrachintha2018@gmail.com>
2023-02-14 09:43:35 +0530

    add function for check if all fields are fill or not

--- Front_End/assets/scripts/AdminDashboardController.js
+++ Front_End/assets/scripts/AdminDashboardController.js
@@ -54,7 +54,7 @@ let regDetails = /^[A-z0-9 &.,/]{4,}$/;
 $('#txtToday').val(today);
 $('#txtTodayDate').val(today);
 
-
+// ----------------------------------------------------------------------------------------------
 // Admin Dashboard Section function
 function getRegisterCustomersCount() {
     $.ajax({
@@ -191,6 +191,7 @@ function loadTodayBookings() {
 }
 
 
+// ----------------------------------------------------------------------------------------------
 // CAR Section
 // Car Reg no
 $('#txtRegNo').on('keyup', function (event) {
@@ -433,7 +434,6 @@ function checkFreeKmForPrice() {
 }
 
 
-
 // free km for the duration
 $('#txtFreeKmForDuration').on('keyup', function (event) {
     var freeKmForDuration = $('#txtFreeKmForDuration').val();
@@ -530,4 +530,245 @@ function checkCompleteKm() {
         $("#txtCompleteKm").css('border', '3px solid red').focus();
         return false;
     }
+}
+
+$('#saveCar').click(function () {
+    if ($('#txtRegNo').val() != "") {
+        if ($('#txtBrand').val() != "") {
+            if ($('#cmbtype').val() != "- Select Car Type -") {
+                if ($('#txtNoOfPassengers').val() != "") {
+                    if ($('#cmbTransmissionType').val() != "- Select Transmission -") {
+                        if ($('#cmbfuel').val() != "- Select Fuel Type -") {
+                            if ($('#cmbColor').val() != "- Select Color -") {
+                                if ($('#imgFrontView').val() != "") {
+                                    if ($('#imgBackView').val() != "") {
+                                        if ($('#imgSideView').val() != "") {
+                                            if ($('#imgInteriorView').val() != "") {
+                                                if ($('#txtDailyRate').val() != "") {
+                                                    if ($('#txtMonthlyRate').val() != "") {
+                                                        if ($('#txtFreeKmForPrice').val() != "") {
+                                                            if ($('#txtFreeKmForDuration').val() != "") {
+                                                                if ($('#txtLossDamageWaiver').val() != "") {
+                                                                    if ($('#txtPriceForExtraKm').val() != "") {
+                                                                        if ($('#txtCompleteKm').val() != "") {
+                                                                            let res = confirm("Do you want to add this Car?");
+                                                                            if (res) {
+                                                                                addCar();
+                                                                            }
+                                                                        } else {
+                                                                            alert("Please enter complete kilometers");
+                                                                        }
+                                                                    } else {
+                                                                        alert("Please enter price for extra km");
+                                                                    }
+                                                                } else {
+                                                                    alert("Please enter loss damage waiver");
+                                                                }
+                                                            } else {
+                                                                alert("Please enter free km for duration");
+                                                            }
+                                                        } else {
+                                                            alert("Please enter free km for price");
+                                                        }
+                                                    } else {
+                                                        alert("Please enter monthly rate");
+                                                    }
+                                                } else {
+                                                    alert("Please enter daily rate");
+                                                }
+                                            } else {
+                                                alert("Please upload interior view image");
+                                            }
+                                        } else {
+                                            alert("Please upload side view image");
+                                        }
+                                    } else {
+                                        alert("Please upload back view image");
+                                    }
+                                } else {
+                                    alert("Please upload front view image");
+                                }
+                            } else {
+                                alert("Please select color");
+                            }
+                        } else {
+                            alert("Please select fuel type");
+                        }
+                    } else {
+                        alert("Please select transmission type");
+                    }
+                } else {
+                    alert("Please enter no of passengers");
+                }
+            } else {
+                alert("Please select car type");
+            }
+        } else {
+            alert("Please enter brand");
+        }
+    } else {
+        alert("Please enter registration No");
+    }
+});
+
+function addCar() {
+    let regNo = $('#txtRegNo').val();
+    let brand = $('#txtBrand').val();
+    let type = $('#cmbtype').find('option:selected').text();
+    let noOfPassengers = $('#txtNoOfPassengers').val();
+    let transmission = $('#cmbTransmissionType').find('option:selected').text();
+    let fuel = $('#cmbfuel').find('option:selected').text();
+    let color = $('#cmbColor').find('option:selected').text();
+    let dailyRate = $('#txtDailyRate').val();
+    let monthlyRate = $('#txtMonthlyRate').val();
+    let freeKmForPrice = $('#txtFreeKmForPrice').val();
+    let freeKmForDuration = $('#txtFreeKmForDuration').val();
+    let lossDamageWavier = $('#txtLossDamageWaiver').val();
+    let priceForExtraKm = $('#txtPriceForExtraKm').val();
+    let completeKm = $('#txtCompleteKm').val();
+    let status = "Available";
+
+    var car = {
+        registrationNO: regNo,
+        brand: brand,
+        type: type,
+        noOfPassengers: noOfPassengers,
+        transmissionType: transmission,
+        fuelType: fuel,
+        color: color,
+        dailyRate: dailyRate,
+        monthlyRate: monthlyRate,
+        freeKmForPrice: freeKmForPrice,
+        freeKmForDuration: freeKmForDuration,
+        lossDamageWaiver: lossDamageWavier,
+        priceForExtraKm: priceForExtraKm,
+        completeKm: completeKm,
+        status: status
+    }
+
+    $.ajax({
+        url: baseUrl + "api/v1/car",
+        method: "POST",
+        contentType: "application/json",
+        data: JSON.stringify(car),
+        success: function (res) {
+            uploadCarImages(regNo);
+            loadAllCars();
+            getAvailableCarCount();
+            swal({
+                title: "Confirmation",
+                text: "Car Added Successfully",
+                icon: "success",
+                button: "Close",
+                timer: 2000
+            });
+        },
+        error: function (ob) {
+            swal({
+                title: "Error!",
+                text: "Car Not Added Successfully",
+                icon: "error",
+                button: "Close",
+                timer: 2000
+            });
+        }
+    })
+}
+
+function uploadCarImages(registrationID) {
+    var fileObjectFront = $('#imgFrontView')[0].files[0];
+    var fileNameFront = registrationID + "-front-" + $('#imgFrontView')[0].files[0].name;
+
+    var fileObjectBack = $('#imgBackView')[0].files[0];
+    var fileNameBack = registrationID + "-back-" + $('#imgBackView')[0].files[0].name;
+
+    var fileObjectSide = $('#imgSideView')[0].files[0];
+    var fileNameSide = registrationID + "-side-" + $('#imgSideView')[0].files[0].name;
+
+    var fileObjectInterior = $('#imgInteriorView')[0].files[0];
+    var fileNameInterior = registrationID + "-interior-" + $('#imgInteriorView')[0].files[0].name;
+
+    var data = new FormData();
+    data.append("frontImg", fileObjectFront, fileNameFront);
+    data.append("backImg", fileObjectBack, fileNameBack);
+    data.append("interImg", fileObjectInterior, fileNameInterior);
+    data.append("sideImg", fileObjectSide, fileNameSide);
+
+    $.ajax({
+        url: baseUrl + "api/v1/car/up/" + registrationID,
+        method: "PUT",
+        async: true,
+        contentType: false,
+        processData: false,
+        data: data,
+        success: function (res) {
+            console.log("Uploaded");
+            clearAddCarFields();
+        }
+    })
+}
+
+function clearAddCarFields() {
+    $('#txtRegNo').val("");
+    $('#txtBrand').val("");
+    $('#txtNoOfPassengers').val("");
+    $('#txtDailyRate').val("");
+    $('#txtMonthlyRate').val("");
+    $('#txtFreeKmForPrice').val("");
+    $('#txtFreeKmForDuration').val("");
+    $('#txtLossDamageWaiver').val("");
+    $('#txtPriceForExtraKm').val("");
+    $('#txtCompleteKm').val("");
+    $('#imgFrontView').val("");
+    $('#imgBackView').val("");
+    $('#imgSideView').val("");
+    $('#imgInteriorView').val("");
+    $('#searchCar').val("");
+    $('#txtRegNo').css('border', '1px solid #ced4da');
+    $('#txtBrand').css('border', '1px solid #ced4da');
+    $('#cmbtype').css('border', '1px solid #ced4da');
+    $('#txtNoOfPassengers').css('border', '1px solid #ced4da');
+    $('#cmbTransmissionType').css('border', '1px solid #ced4da');
+    $('#cmbfuel').css('border', '1px solid #ced4da');
+    $('#cmbColor').css('border', '1px solid #ced4da');
+    $('#txtDailyRate').css('border', '1px solid #ced4da');
+    $('#txtMonthlyRate').css('border', '1px solid #ced4da');
+    $('#txtFreeKmForPrice').css('border', '1px solid #ced4da');
+    $('#txtFreeKmForDuration').css('border', '1px solid #ced4da');
+    $('#txtLossDamageWaiver').css('border', '1px solid #ced4da');
+    $('#txtPriceForExtraKm').css('border', '1px solid #ced4da');
+    $('#txtCompleteKm').css('border', '1px solid #ced4da');
+    $('#searchCar').css('border', '1px solid #ced4da');
+
+    $("#imgFrontView").prop('disabled', false);
+    $("#imgBackView").prop('disabled', false);
+    $("#imgSideView").prop('disabled', false);
+    $("#imgInteriorView").prop('disabled', false);
+    $("#updateCar").prop('disabled', true);
+    $("#delCar").prop('disabled', true);
+    $("#saveCar").prop('disabled', false);
+}
+
+$('#clearCar').click(function () {
+    clearAddCarFields();
+    loadAllCars();
+});
+
+function loadAllCars() {
+    $('#carTable').empty();
+    $.ajax({
+        url: baseUrl + "api/v1/car",
+        method: "GET",
+        success: function (res) {
+            for (const car of res.data) {
+                let row = `<tr><td>${car.registrationNO}</td><td>${car.brand}</td><td>${car.type}</td><td>${car.noOfPassengers}</td><td>${car.transmissionType}</td><td>${car.fuelType}</td><td>${car.color}</td><td>${car.dailyRate}</td><td>${car.monthlyRate}</td><td>${car.freeKmForPrice}</td><td>${car.freeKmForDuration}</td><td>${car.lossDamageWaiver}</td><td>${car.priceForExtraKm}</td><td>${car.completeKm}</td><td>${car.status}</td></tr>`;
+                $('#carTable').append(row);
+            }
+            bindCarTableClickEvents();
+        }
+    });
+}
+
+function bindCarTableClickEvents(){
+
 }
