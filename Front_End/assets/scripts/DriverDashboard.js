d712d4564919e99350b1eb342fbe2c10c3d418c0
kasunweerasinghe <kasunrachintha2018@gmail.com>
2023-02-14 16:14:34 +0530

    add function for load data into driver table

--- Front_End/assets/scripts/DriverDashboard.js
+++ Front_End/assets/scripts/DriverDashboard.js
@@ -5,10 +5,41 @@ function getLastLogUser() {
     $.ajax({
         url: "http://localhost:8080/Back_End_war/api/v1/login/getLastLogin",
         method: "GET",
-        success:function (res){
+        success: function (res) {
             let login = res.data;
             console.log(login);
-            getAllDriverData(login.username,login.password);
+            getAllDriverData(login.username, login.password);
         }
     });
 }
+
+
+// function for get driver data
+function getAllDriverData(username, password) {
+    $.ajax({
+        url: "http://localhost:8080/Back_End_war/api/v1/driver/set/" + username + "/" + password,
+        method: "GET",
+        success: function (res) {
+            let driver = res.data;
+            console.log(driver);
+            loadDriverSchedule(driver.licenceNo);
+        }
+    })
+}
+
+// load data into driver table
+function loadDriverSchedule(licenceNo){
+    $('#bookingScheduleTable').empty();
+    let status = "Accepted";
+    $.ajax({
+        url: "http://localhost:8080/Back_End_war/api/v1/CarRent/getCarRents/" + status + "/" + licenceNo,
+        method:"GET",
+        success:function (res) {
+            for (let carRent of res.data) {
+                let row = `<tr><td>${carRent.customer.customerId}</td><td>${carRent.customer.name}</td><td>${carRent.customer.contactNo}</td><td>${carRent.rentId}</td><td>${carRent.car.registrationNO}</td><td>${carRent.pickUpDate}</td><td>${carRent.returnDate}</td></tr>`;
+                $('#bookingScheduleTable').append(row);
+            }
+        }
+    })
+}
+
