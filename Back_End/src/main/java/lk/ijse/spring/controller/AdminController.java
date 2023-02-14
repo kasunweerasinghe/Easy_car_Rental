e66b0f7dc19b2c880c7c93ceb7923517e5bf867c
kasunweerasinghe <kasunrachintha2018@gmail.com>
2023-02-14 08:27:15 +0530

    add admin contoller and service impl

--- Back_End/src/main/java/lk/ijse/spring/controller/AdminController.java
+++ Back_End/src/main/java/lk/ijse/spring/controller/AdminController.java
@@ -7,12 +7,65 @@
 
 package lk.ijse.spring.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lk.ijse.spring.dto.AdminDTO;
+import lk.ijse.spring.service.AdminService;
+import lk.ijse.spring.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/admin")
 @CrossOrigin
 public class AdminController {
+
+    @Autowired
+    AdminService service;
+
+    @GetMapping
+    public ResponseUtil getAllAdmins() {
+        return new ResponseUtil(200, "Ok", service.getAllAdmins());
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public ResponseUtil saveAdmin(@RequestBody AdminDTO dto) {
+//        System.out.println(dto.toString());
+        service.saveAdmin(dto);
+        return new ResponseUtil(200, "Saved", null);
+    }
+
+    @PutMapping
+    public ResponseUtil updateAdmin(@RequestBody AdminDTO dto) {
+        service.updateAdmin(dto);
+        return new ResponseUtil(200, "Updated", null);
+    }
+
+    @DeleteMapping(params = {"id"})
+    public ResponseUtil deleteAdmin(@RequestParam String id) {
+        service.deleteAdmin(id);
+        return new ResponseUtil(200, "Deleted", null);
+    }
+
+    @GetMapping(path = "/{username}/{password}")
+    public ResponseUtil searchAdminByUsernameAndPassword(@PathVariable String username, @PathVariable String password) {
+        if (service.findAdminByUserName(username)) {
+            if (service.findAdminByPassWord(password)) {
+                return new ResponseUtil(200, "Login Successful", true);
+            } else {
+                return new ResponseUtil(404, "Incorrect Password", false);
+            }
+        } else {
+            return new ResponseUtil(404, "Incorrect Username", false);
+        }
+    }
+
+    @GetMapping(path = "/generateAdminID")
+    public ResponseUtil generateAdminId() {
+        return new ResponseUtil(200, "Ok", service.generateAdminId());
+    }
+
+
+
 }
