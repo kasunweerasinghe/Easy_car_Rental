6d12d773bfde2373d4fc95e0ca497b80ed43a104
kasunweerasinghe <kasunrachintha2018@gmail.com>
2023-02-21 10:08:48 +0530

    add car rent classes

---
+++ Back_End/src/main/java/lk/ijse/spring/service/impl/CarRentServiceImpl.java
@@ -0,0 +1,11 @@
+/**
+ * created by kasun weerasinghe
+ * Date: 2023-02-21
+ * Time: 10:08
+ * Project Name: Back_End
+ */
+
+package lk.ijse.spring.service.impl;
+
+public class CarRentServiceImpl {
+}
