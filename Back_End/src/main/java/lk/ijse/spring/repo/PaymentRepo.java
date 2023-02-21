40cbd7289c0192601a92fc12ba2db2486259aaed
kasunweerasinghe <kasunrachintha2018@gmail.com>
2023-02-21 11:15:33 +0530

    add paymentController and customer can rent a car successfully

---
+++ Back_End/src/main/java/lk/ijse/spring/repo/PaymentRepo.java
@@ -0,0 +1,30 @@
+package lk.ijse.spring.repo;
+
+import lk.ijse.spring.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface PaymentRepo extends JpaRepository<Payment, String> {
+    @Query(value = "SELECT * FROM Payment WHERE date BETWEEN :fromDate AND :toDate", nativeQuery = true)
+    List<Payment> getAllPaymentsByDateRange(@Param("fromDate") String fromDate, @Param("toDate") String toDate);
+
+
+    @Query(value = "SELECT * FROM Payment WHERE customerId=:customerId", nativeQuery = true)
+    List<Payment> getAllPaymentsByCustomerId(@Param("customerId") String customerId);
+
+    @Query(value = "SELECT paymentId FROM Payment ORDER BY paymentId DESC LIMIT 1", nativeQuery = true)
+    String generatePaymentId();
+
+    boolean existsByRentalRentId(String rentId);
+
+    void deleteAllByRentalRentId(String rentID);
+
+    @Query(value = "SELECT SUM(amount) FROM Payment WHERE rentID=:rentID",nativeQuery = true)
+    double getSumOfPaidPayments(@Param("rentID") String rentID);
+
+    @Query(value = "SELECT SUM(amount) FROM Payment WHERE date BETWEEN :fromDate AND :toDate",nativeQuery = true)
+    double getSumOfPaymentAmount(@Param("fromDate") String fromDate,@Param("toDate") String toDate);
+}
