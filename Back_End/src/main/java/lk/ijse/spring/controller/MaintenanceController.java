5305a3e22f6b232e3969620d09f24d2d901b65bb
kasunweerasinghe <kasunrachintha2018@gmail.com>
2023-02-27 13:32:00 +0530

    add maintenence

--- Back_End/src/main/java/lk/ijse/spring/controller/MaintenanceController.java
+++ Back_End/src/main/java/lk/ijse/spring/controller/MaintenanceController.java
@@ -7,12 +7,73 @@
 
 package lk.ijse.spring.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lk.ijse.spring.dto.MaintenanceDTO;
+import lk.ijse.spring.service.MaintenanceService;
+import lk.ijse.spring.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/maintenance")
 @CrossOrigin
 public class MaintenanceController {
+    @Autowired
+    MaintenanceService service;
+
+    @GetMapping
+    public ResponseUtil getAllMaintenance() {
+        return new ResponseUtil(200, "Ok", service.getAllMaintenances());
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public ResponseUtil addMaintenance(@RequestBody MaintenanceDTO dto) {
+        System.out.println(dto.toString());
+        service.addMaintenance(dto);
+        return new ResponseUtil(200, "Saved", null);
+    }
+
+    @PutMapping
+    public ResponseUtil updateMaintenance(@RequestBody MaintenanceDTO dto) {
+        service.updateMaintenance(dto);
+        return new ResponseUtil(200, "Updated", null);
+    }
+
+    @DeleteMapping(params = {"maintenanceId"})
+    public ResponseUtil deleteMaintenance(@RequestParam String maintenanceId) {
+        service.deleteMaintenance(maintenanceId);
+        return new ResponseUtil(200, "Deleted", null);
+    }
+
+    @GetMapping(path = "/{maintenanceId}")
+    public ResponseUtil searchMaintenance(@PathVariable String maintenanceId) {
+        return new ResponseUtil(200, "Ok", service.searchMaintenance(maintenanceId));
+    }
+
+    @GetMapping(path = "/generateMaintenanceId")
+    public ResponseUtil generateMaintenanceId() {
+        return new ResponseUtil(200, "Ok", service.generateMaintenanceId());
+    }
+
+    @PutMapping(path = "/{maintenanceId}/{cost}")
+    public ResponseUtil updateMaintenanceCost(@PathVariable String maintenanceId, @PathVariable double cost) {
+        service.updateMaintenanceCost(maintenanceId, cost);
+        return new ResponseUtil(200, "Updated", null);
+    }
+
+    @GetMapping(path = "/underMaintenances")
+    public ResponseUtil getAllUnderMaintenances() {
+        return new ResponseUtil(200, "Ok", service.getAllUnderMaintenances());
+    }
+
+    @GetMapping(path = "/getAll/{fromDate}/{toDate}")
+    public ResponseUtil getAllMaintenancesByDateRange(@PathVariable String fromDate, @PathVariable String toDate) {
+        return new ResponseUtil(200, "Ok", service.getAllMaintenancesByDateRange(fromDate, toDate));
+    }
+
+    @GetMapping(path = "/sum/{fromDate}/{toDate}")
+    public ResponseUtil getSumOfMaintenanceAmount(@PathVariable String fromDate, @PathVariable String toDate) {
+        return new ResponseUtil(200, "Ok", service.getSumOfMaintenanceAmount(fromDate, toDate));
+    }
 }
